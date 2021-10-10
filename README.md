8652f7e00fbd3b78d8c3acefdc6ec7c57e6ad6e9
johnherrera88 <john.herrera88@gmail.com>
2021-10-10 11:39:49 -0600

    final

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 #Make Change Project
 
 ## Description
-This is my very first Java project.  put a descriptive Description of what the project does and how it works.
+This is my very first Java project.  I've created a cash register that accepts exact change and informs the customer if they did not provide enough money for their purchase.  The cash register also provides change back if the customer provided more money than the total cost of their items.  
 
-## What I learned
-I learned to understand this and that.  How things work
+## How it Works
+My main method uses the scanner to get input from the customer to establish the cost of the item and the amount of money they are using to complete the purchase.  If the customer provides the exact amount of the purchase price the sequence ends.  If they do not provide enough money to complete the purchase they receive an error.  
 
-## Technologies used
-i.e. Java, Eclipse, Git
+When the customer provides too much money they will receive change back.  When change is due back to the customer I wanted to call on another method that encompasses the mathematical equation that determines how much change the customer gets back.  The equations also ensures the customer receives their change back in the highest denomination of bills and coins possible.  The math is based on the amount of pennies in each respective dollar and coin amount.  
+
+## What I Learned
+I learned that calling on myMethod from main required required me to first establish the exact change due and then push that amount down to myMethod.  Initially, I did not do this which caused my change equations to be useless. They were not receiving the exactChange input from main which is my baseline for computing the denominations that are returned back to the customer.
+
+## Technologies Used
+Java, Eclipse, Git, terminal, atom.
--- src/com/Skilldistillery/app/MakeChange.java
+++ src/com/Skilldistillery/app/MakeChange.java
@@ -1,9 +1,101 @@
 package com.Skilldistillery.app;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Double cost = 0.0;
+		Double tendered = 0.0;
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Hello, what is the price of your item?");
+		cost = scanner.nextDouble();
+
+		System.out.println("Thanks!  What is the amount tendered?");
+		tendered = scanner.nextDouble();
+
+		double exactChange = tendered - cost;
+
+		while (exactChange == 0) {
+			System.out.println("Thank you for using exact change!");
+			break;
+		}
+
+		if (cost > tendered) {
+			System.out.println("Whoops, looks like you did not give me enough money.");
+		}
+
+		if (cost < tendered) {
+			System.out.println("Your change is: ");
+			myMethod(exactChange);
+
+		}
+		scanner.close();
+	}
+
+	public static void myMethod(double theChange) {
+
+		int change = (int) (theChange * 100);
+
+		int twentyDollarCount = 0;
+		if (change > 2000) {
+			twentyDollarCount = change / 2000;
+			change = change % 2000;
+			System.out.print(twentyDollarCount + " twenty dollar bill, ");
+		}
+
+		int tenDollarCount = 0;
+		if (change > 1000) {
+			tenDollarCount = change / 1000;
+			change = change % 1000;
+			System.out.print(tenDollarCount + " ten dollar bill, ");
+		}
+
+		int fiveDollarCount = 0;
+		if (change > 500) {
+			fiveDollarCount = change / 500;
+			change = change % 500;
+			System.out.print(fiveDollarCount + " five dollar bill, ");
+		}
+
+		int dollarCount = 0;
+		if (change > 100) {
+			dollarCount = change / 100;
+			change = change % 100;
+			System.out.print(dollarCount + " dollar bill, ");
+		}
+
+		int quarterCount = 0;
+		if (change > 25) {
+			quarterCount = change / 25;
+			change = change % 25;
+			System.out.print(quarterCount + " quarter, ");
+		}
+
+		int dimeCount = 0;
+		if (change > 10) {
+			dimeCount = change / 10;
+			change = change % 10;
+			System.out.print(dimeCount + " dime, ");
+		}
+
+		int nickelCount = 0;
+		if (change > 5) {
+			nickelCount = change / 5;
+			change = change % 5;
+			System.out.print(nickelCount + " nickel, ");
+		}
+
+		int pennyCount = 0;
+		if (change >= 1) {
+			pennyCount = change / 1;
+			change = change % 1;
+			System.out.print(pennyCount + " pennies. ");
+
+		}
 
 	}
 
